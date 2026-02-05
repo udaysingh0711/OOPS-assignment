76fe0495d14293e3b92e7f05ba51206c03d01763
udaysingh0711 <usingh1_be24@thapar.edu>
2026-02-05 14:29:42 +0530

    Create question3.cpp

---
+++ assignment1/question3.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+int x, y;
+
+cout << "Enter first number: ";
+cin >> x;
+cout<<"Enter second number: ";
+cin>>y;
+cout << "Addition: " << x + y << endl;
+cout << "Subtraction: " << x - y << endl;
+cout << "Multiplication: " << x * y << endl;
+cout << "Division: " << x / y << endl;
+
+return 0;
+}
