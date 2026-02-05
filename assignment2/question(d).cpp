153dd7ec310393b04914f7a312d23a99f858fdcb
udaysingh0711 <usingh1_be24@thapar.edu>
2026-02-05 14:46:19 +0530

    Create question(d).cpp

---
+++ assignment2/question(d).cpp
@@ -0,0 +1,9 @@
+//QUESTION 6- Implement scope resolution operator : : for the following uses
+//(d)	Use inbuilt libraries (cin cout with scope resolution operator)
+
+#include<iostream>
+int main()
+{
+std::cout<<"Using cout with scope resolution operator";
+return 0;
+}
