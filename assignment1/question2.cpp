78fe27dd14d59ea66a686e14fede31353afabdef
udaysingh0711 <usingh1_be24@thapar.edu>
2026-02-05 14:28:56 +0530

    Create question2.cpp

---
+++ assignment1/question2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    float b;
+    char c;
+
+    cout << "Enter integer: ";
+    cin >> a;
+    cout << "Enter decimal: ";
+    cin >> b;
+    cout << "Enter character: ";
+    cin >> c;
+    cout << "Integer: " << a << endl;
+    cout << "Decimal: " << b << endl;
+    cout << "Character: " << c << endl;
+    return 0;
+}
