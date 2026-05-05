3a6d1e9e1f4518b92c398d7ffd233279926a9f45
udaysingh0711 <usingh1_be24@thapar.edu>
2026-05-05 17:44:42 +0530

    Create q3.cpp

---
+++ assignmnet3/q3.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class B;
+
+class A {
+    int x;
+
+public:
+    A(int x) {
+        this->x = x;
+    }
+
+    friend int addValues(A, B);
+
+    void show() {
+        cout << "A = " << x << endl;
+    }
+};
+
+class B {
+    int y;
+
+public:
+    B(int y) {
+        this->y = y;
+    }
+
+    friend int addValues(A, B);
+
+    void show() {
+        cout << "B = " << y << endl;
+    }
+};
+
+int addValues(A a, B b) {
+    return a.x + b.y;
+}
+
+int main() {
+    A objA(15);
+    B objB(25);
+
+    cout << "Object values:" << endl;
+    objA.show();
+    objB.show();
+
+    int sum = addValues(objA, objB);
+
+    cout << endl << "Sum = " << sum << endl;
+
+    return 0;
+}
